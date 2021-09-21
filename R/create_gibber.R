f1b0a4e1ade1ed8f98d2c05bafe195ba2f2c0440
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2021-09-21 18:20:35 +0200

    add license, function and documentation

---
+++ R/create_gibber.R
@@ -0,0 +1,23 @@
+#' A function for random word generation.
+#'
+#' Compute a word of length n consisting of randomly chosen
+#' characters from the alphabet.
+#'
+#' @param n a numeric vector giving the number of characters in each word.
+#' @param prob a vector of probability weights for obtaining the elements of the vector being sampled.
+#'
+#' @return a character vector.
+#' @export
+#'
+#' @examples
+#' # generate a random word of length 5
+#' create_gibber(n=5)
+#' # generate three randoms word:
+#' create_gibber(c(5, 7, 9))
+create_gibber <- function(n, prob=NULL){
+  nonsense_words <- sapply(n, function(x){
+    paste(sample(letters, size = x, replace = TRUE, prob = prob), collapse = "")
+  })
+  return(nonsense_words)
+}
+
