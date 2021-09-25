880970ce31add9747378a4e6156b3e6da795ce04
Glenn Hiemstra <glenn.hiemstra@gmail.com>
2021-09-25 19:57:08 +0200

    adjust is_gibber, assure model doesn'nt load unncessary

--- R/is_gibber.R
+++ R/is_gibber.R
@@ -1,6 +1,14 @@
-#' Predict presence of gibberish words in a sentence
+# Any work that initializes a python object needs to happen at package load time, not package build time.
+# Thus we do the following initialization step
+model <- NULL
+.onLoad <- function(libname, pkgname) {
+  path <- system.file("extdata", "gibber_model.h5", package = "gibber", mustWork = TRUE)
+  model <<- keras::load_model_hdf5(path)
+}
+
+#' Predict presence of gibberish words
 #'
-#' A recurrent neural network is used to calculate the probability of
+#' Blabla..
 #'
 #' @param words character vector
 #'
@@ -10,10 +18,6 @@
 #' words <- c("asdf", "elephant")
 #' is_gibber(words)
 is_gibber <- function(words){
-
-  # load tokenizer and model
-  path <- system.file("extdata", "gibber_model.h5", package = "gibber", mustWork = TRUE)
-  model <- keras::load_model_hdf5(path)
 
   # convert words to matrix
   matrix <- tokenize_words(words)
